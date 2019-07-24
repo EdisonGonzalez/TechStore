197d6dd5d46a6b315105c0086db63b77fea9c22d
Gonzalez, Edison <edison.gonzalez@accenture.com>
2019-07-24 14:44:02 -0500

    Agregando cambios al navbar para navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CatalogoComponent } from './component/catalogo/catalogo.component';
+import { ProductosComponent } from './component/productos/productos.component';
 
 const routes: Routes = [
-  {path: '', component: CatalogoComponent},
   {path: 'catalogo', component: CatalogoComponent},
+  {path: 'producto', component: ProductosComponent},
   {path: '**', redirectTo: 'catalogo'},
-  { path: '', redirectTo: 'catalogo', pathMatch: 'full' },
+  {path: '', redirectTo: 'catalogo', pathMatch: 'full'},
 ];
 
 @NgModule({
